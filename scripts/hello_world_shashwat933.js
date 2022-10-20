82485299386de90d2ae751ec7a7188e444bd5480
Shashwat <shashwatgupta21054@gmail.com>
2022-10-20 07:10:14 +0530

    Did some changes

--- scripts/hello_world_shashwat933.js
+++
@@ -1,6 +0,0 @@
-// LANGUAGE: Javascript
-// ENV: Node.js
-// AUTHOR: Shashwat Gupta
-// GITHUB: https://github.com/shashwat933
-
-console.log("Hello World !!");
---
+++ scripts/hello_world_shashwat933.py
@@ -0,0 +1,5 @@
+# LANGUAGE: Python
+# AUTHOR: Shashwat Gupta
+# GITHUB: https://github.com/shashwat933
+
+print("Hello, World!");
